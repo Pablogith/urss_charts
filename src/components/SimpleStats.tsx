169cd7011d712917a6f181e50b21c1a243d42382
Pawel Relinski <pawelrelinski@gmail.com>
2023-02-19 14:25:22 +0100

    feat: Create URSS Charts

--- src/components/SimpleStats.tsx
+++ src/components/SimpleStats.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 
-export const SimpleStats = () => {
+interface Props {
+  allFillings: number;
+  averageNumberOfFills: number;
+  maximumNumberOfFills: number;
+}
+
+export const SimpleStats = (props: Props) => {
+  const {
+    allFillings,
+    averageNumberOfFills,
+    maximumNumberOfFills } = props;
+
   return (
     <>
       <div>
         <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
             <dt className="truncate text-sm font-medium text-gray-500">Wszystkie wypełnienia</dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">71,897</dd>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+              { allFillings }
+            </dd>
           </div>
 
           <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
             <dt className="truncate text-sm font-medium text-gray-500">Średnia liczba wypełnień</dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">5816</dd>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+              { averageNumberOfFills }
+            </dd>
           </div>
 
           <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
             <dt className="truncate text-sm font-medium text-gray-500">Maks. liczba wypełnień</dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">2457</dd>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+              { maximumNumberOfFills }
+            </dd>
           </div>
         </dl>
       </div>
